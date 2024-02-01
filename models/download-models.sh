021268f62cda7bef2474865bd90841fd34f812e9
Tony Tam <tony@eatbacon.org>
2024-01-31 18:07:17 -0800

    fixed download script to work on mac

--- models/download-models.sh
+++ models/download-models.sh
@@ -1,11 +1,11 @@
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.weights
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.cfg -o cross-hands.cfg
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.weights -o cross-hands.weights
 
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.weights
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.cfg -o cross-hands-tiny.cfg
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.weights -o cross-hands-tiny.weights
 
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.weights
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.cfg -o cross-hands-tiny-prn.cfg
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.weights -o cross-hands-tiny-prn.weights
 
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.weights
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.cfg -o cross-hands-yolov4-tiny.cfg
+curl -L https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.weights -o cross-hands-yolov4-tiny.weights
